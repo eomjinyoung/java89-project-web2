a226dbaf65f942f5e54018d4c5d311b7f5e9517a
eomjinyoung <jinyoung.eom@gmail.com>
2017-01-31 16:20:33 +0900

    .

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,7 +1,25 @@
 window.addEventListener("load", function() {
 	// header.html을 가져와서 붙인다.
 	get('../header.html', function(result) {
-	  document.querySelector('#header').innerHTML = result;
+	  // 서버에서 로그인 사용자 정보를 가져온다.
+	  get('../auth/loginUser.json', function(jsonText) {
+		var ajaxResult = JSON.parse(jsonText);
+		
+		document.querySelector('#header').innerHTML = result;
+
+		if (ajaxResult.status == "fail") { // 로그인 되지 않았으면,
+			// 로그온 상태 출력 창을 감춘다.
+			document.querySelector('#logon-div').style.display = 'none';
+			return;
+		}
+		
+		// 로그인 되었으면, 로그오프 상태 출력 창을 감춘다. 
+		document.querySelector('#logoff-div').style.display = 'none';
+		document.querySelector('#logon-div img').src = 
+			'../upload/' + ajaxResult.data.photoPath;
+		document.querySelector('#logon-div span').textContent = 
+			ajaxResult.data.name;
+	  });
 	});
 	
 	// sidebar.html을 가져와서 붙인다.
